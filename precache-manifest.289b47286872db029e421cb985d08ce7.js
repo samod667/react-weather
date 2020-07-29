a94ee623f3387c0871ec8be21c958590f501a1e3
dorsamoha <dorsamoha@gmail.com>
2020-07-29 14:15:43 +0300

    Updates

---
+++ precache-manifest.289b47286872db029e421cb985d08ce7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a75cd2f05d9e238e95375b3f1a45006",
+    "url": "/react-weather/index.html"
+  },
+  {
+    "revision": "d007eee99aab9b4afed3",
+    "url": "/react-weather/static/css/main.cc6ffa19.chunk.css"
+  },
+  {
+    "revision": "7500b8e2b88d578e501d",
+    "url": "/react-weather/static/js/2.eb7946c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather/static/js/2.eb7946c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d007eee99aab9b4afed3",
+    "url": "/react-weather/static/js/main.be87383f.chunk.js"
+  },
+  {
+    "revision": "e9e1361e93ffdeb86097",
+    "url": "/react-weather/static/js/runtime-main.6329d675.js"
+  }
+]);
